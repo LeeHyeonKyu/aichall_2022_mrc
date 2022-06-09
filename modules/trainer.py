9dee8ab20c2395812e9d984205fc808c71bbed81
PJHgh <51353039+PJHgh@users.noreply.github.com>
2022-06-09 21:31:33 +0900

    Black and Isort

--- modules/trainer.py
+++ modules/trainer.py
@@ -1,25 +1,28 @@
-import pandas as pd
-import numpy as np
-import torch
 import os
-
 from time import time
+
+import numpy as np
+import pandas as pd
+import torch
 from tqdm import tqdm
+
 # from apex import amp
 
-class Trainer():
 
-    def __init__(self,
-                 model,
-                 optimizer,
-                 loss,
-                 metrics,
-                 device,
-                 logger,
-                 amp,
-                 tokenizer,
-                 interval=100):
-        
+class Trainer:
+    def __init__(
+        self,
+        model,
+        optimizer,
+        loss,
+        metrics,
+        device,
+        logger,
+        amp,
+        tokenizer,
+        interval=100,
+    ):
+
         self.model = model
         self.optimizer = optimizer
         self.loss = loss
@@ -32,42 +35,43 @@ class Trainer():
 
         # History
         self.loss_sum = 0  # Epoch loss sum
-        self.loss_mean = 0 # Epoch loss mean
+        self.loss_mean = 0  # Epoch loss mean
         self.y = list()
         self.y_preds = list()
         self.score_dict = dict()  # metric score
         self.elapsed_time = 0
-        
 
     def train(self, dataloader, epoch_index=0):
-        
+
         start_timestamp = time()
         self.model.train()
- 
+
         for batch_index, batch in enumerate(tqdm(dataloader, leave=True)):
-            
+
             # initialize calculated gradients (from prev step)
             self.optimizer.zero_grad()
             # pull all the tensor batches required for training
-            input_ids = batch['input_ids'].to(self.device)
-            attention_mask = batch['attention_mask'].to(self.device)
-            start_positions = batch['start_positions'].to(self.device)
-            end_positions = batch['end_positions'].to(self.device)
-            
+            input_ids = batch["input_ids"].to(self.device)
+            attention_mask = batch["attention_mask"].to(self.device)
+            start_positions = batch["start_positions"].to(self.device)
+            end_positions = batch["end_positions"].to(self.device)
+
             # train model on batch and return outputs (incl. loss)
             # Inference
-            outputs = self.model(input_ids, attention_mask=attention_mask,
-                            start_positions=start_positions,
-                            end_positions=end_positions)
-            
+            outputs = self.model(
+                input_ids,
+                attention_mask=attention_mask,
+                start_positions=start_positions,
+                end_positions=end_positions,
+            )
+
             loss = outputs.loss
             start_score = outputs.start_logits
             end_score = outputs.end_logits
-            
-            
+
             start_idx = torch.argmax(start_score, dim=1).cpu().tolist()
             end_idx = torch.argmax(end_score, dim=1).cpu().tolist()
-            
+
             # Update
             if self.amp is None:
                 loss.backward()
@@ -75,26 +79,31 @@ class Trainer():
             else:
                 with self.amp.scale_loss(loss, self.optimizer) as scaled_loss:
                     scaled_loss.backward()
-                
+
                 self.optimizer.step()
-                            
+
             # History
             self.loss_sum += loss.item()
-            
+
             # create answer; list of strings
             for i in range(len(input_ids)):
                 if start_idx[i] > end_idx[i]:
-                    output = ''
-                
-                self.y_preds.append(self.tokenizer.decode(input_ids[i][start_idx[i]:end_idx[i]]))
-                self.y.append(self.tokenizer.decode(input_ids[i][start_positions[i]:end_positions[i]]))
+                    output = ""
 
+                self.y_preds.append(
+                    self.tokenizer.decode(input_ids[i][start_idx[i] : end_idx[i]])
+                )
+                self.y.append(
+                    self.tokenizer.decode(
+                        input_ids[i][start_positions[i] : end_positions[i]]
+                    )
+                )
 
             # Logging
             if batch_index % self.interval == 0:
                 msg = f"batch: {batch_index}/{len(dataloader)} loss: {loss.item()}"
                 self.logger.info(msg)
-                
+
         # Epoch history
         self.loss_mean = self.loss_sum / len(dataloader)  # Epoch loss mean
 
@@ -109,43 +118,51 @@ class Trainer():
 
     @torch.no_grad()
     def validate(self, dataloader, epoch_index=0):
-        
+
         start_timestamp = time()
         self.model.eval()
- 
+
         for batch_index, batch in enumerate(tqdm(dataloader, leave=True)):
             # pull all the tensor batches required for training
-            input_ids = batch['input_ids'].to(self.device)
-            attention_mask = batch['attention_mask'].to(self.device)
-            start_positions = batch['start_positions'].to(self.device)
-            end_positions = batch['end_positions'].to(self.device)
-            
+            input_ids = batch["input_ids"].to(self.device)
+            attention_mask = batch["attention_mask"].to(self.device)
+            start_positions = batch["start_positions"].to(self.device)
+            end_positions = batch["end_positions"].to(self.device)
+
             # Inference
-            outputs = self.model(input_ids, attention_mask=attention_mask,
-                            start_positions=start_positions,
-                            end_positions=end_positions)
-            
+            outputs = self.model(
+                input_ids,
+                attention_mask=attention_mask,
+                start_positions=start_positions,
+                end_positions=end_positions,
+            )
+
             loss = outputs.loss
             start_score = outputs.start_logits
             end_score = outputs.end_logits
-            
-            
+
             start_idx = torch.argmax(start_score, dim=1).cpu().tolist()
             end_idx = torch.argmax(end_score, dim=1).cpu().tolist()
 
             # History
             self.loss_sum += loss.item()
-            
+
             # create answer; list of strings
             for i in range(len(input_ids)):
-                self.y_preds.append(self.tokenizer.decode(input_ids[i][start_idx[i]:end_idx[i]]))
-                self.y.append(self.tokenizer.decode(input_ids[i][start_positions[i]:end_positions[i]]))
+                self.y_preds.append(
+                    self.tokenizer.decode(input_ids[i][start_idx[i] : end_idx[i]])
+                )
+                self.y.append(
+                    self.tokenizer.decode(
+                        input_ids[i][start_positions[i] : end_positions[i]]
+                    )
+                )
 
             # Logging
             if batch_index % self.interval == 0:
                 msg = f"batch: {batch_index}/{len(dataloader)} loss: {loss.item()}"
                 self.logger.info(msg)
-                
+
         # Epoch history
         self.loss_mean = self.loss_sum / len(dataloader)  # Epoch loss mean
 
@@ -156,7 +173,7 @@ class Trainer():
 
         # Elapsed time
         end_timestamp = time()
-        self.elapsed_time = end_timestamp - start_timestamp        
+        self.elapsed_time = end_timestamp - start_timestamp
 
     def clear_history(self):
         self.loss_sum = 0
@@ -166,6 +183,3 @@ class Trainer():
         self.score_dict = dict()
         self.elapsed_time = 0
         torch.cuda.empty_cache()
-        
-        
-        
--- train.py
+++ train.py
@@ -1,27 +1,24 @@
-from modules.utils import load_yaml, save_yaml, get_logger
-
-from modules.earlystoppers import EarlyStopper
-from modules.recorders import Recorder
-from modules.datasets import QADataset
-from modules.trainer import Trainer
-
-from modules.optimizers import get_optimizer
-from modules.metrics import get_metric
-from modules.losses import get_loss
-from models.utils import get_model
-from modules.preprocessing import get_tokenizer
-
-from torch.utils.data import DataLoader
-import torch
-
-from datetime import datetime, timezone, timedelta
-import numpy as np
-import random
-import os
-import copy
 import argparse
+import copy
+import os
+import random
+from datetime import datetime, timedelta, timezone
 
+import numpy as np
+import torch
 import wandb
+from torch.utils.data import DataLoader
+
+from models.utils import get_model
+from modules.datasets import QADataset
+from modules.earlystoppers import EarlyStopper
+from modules.losses import get_loss
+from modules.metrics import get_metric
+from modules.optimizers import get_optimizer
+from modules.preprocessing import get_tokenizer
+from modules.recorders import Recorder
+from modules.trainer import Trainer
+from modules.utils import get_logger, load_yaml, save_yaml
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--train_cfg", type=str, default="train_config.yml")
@@ -31,7 +28,7 @@ args = parser.parse_args()
 PROJECT_DIR = os.path.dirname(__file__)
 
 # Load config
-config_path = os.path.join(PROJECT_DIR, 'config', args.train_cfg)
+config_path = os.path.join(PROJECT_DIR, "config", args.train_cfg)
 config = load_yaml(config_path)
 
 # Train Serial
@@ -39,77 +36,96 @@ kst = timezone(timedelta(hours=9))
 train_serial = datetime.now(tz=kst).strftime("%Y%m%d_%H%M%S")
 
 # Recorder directory
-DEBUG = config['TRAINER']['debug']
-print(f'debug {DEBUG}')
+DEBUG = config["TRAINER"]["debug"]
+print(f"debug {DEBUG}")
 
-RECORDER_DIR = os.path.join(PROJECT_DIR, 'results', 'train', train_serial)
+RECORDER_DIR = os.path.join(PROJECT_DIR, "results", "train", train_serial)
 os.makedirs(RECORDER_DIR, exist_ok=True)
 
 # Data directory
-DATA_DIR = config['DIRECTORY']['dataset']
-PREPROCESSED_DIR = os.path.join(DATA_DIR, 'preprocessed', config['TRAINER']['pretrained'])
+DATA_DIR = config["DIRECTORY"]["dataset"]
+PREPROCESSED_DIR = os.path.join(
+    DATA_DIR, "preprocessed", config["TRAINER"]["pretrained"]
+)
 
 # Seed
-torch.manual_seed(config['TRAINER']['seed'])
+torch.manual_seed(config["TRAINER"]["seed"])
 torch.backends.cudnn.deterministic = True
 torch.backends.cudnn.benchmark = False
-np.random.seed(config['TRAINER']['seed'])
-random.seed(config['TRAINER']['seed'])
+np.random.seed(config["TRAINER"]["seed"])
+random.seed(config["TRAINER"]["seed"])
 
 # GPU
-os.environ['CUDA_VISIBLE_DEVICES'] = str(config['TRAINER']['gpu'])
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+os.environ["CUDA_VISIBLE_DEVICES"] = str(config["TRAINER"]["gpu"])
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-if __name__ == '__main__': 
+if __name__ == "__main__":
     """
     00. Set Logger
     """
-    logger = get_logger(name='train', dir_=RECORDER_DIR, stream=False)
+    logger = get_logger(name="train", dir_=RECORDER_DIR, stream=False)
     logger.info(f"Set Logger {RECORDER_DIR}")
-    print('/'.join(logger.handlers[0].baseFilename.split('/')[:-1]))
+    print("/".join(logger.handlers[0].baseFilename.split("/")[:-1]))
 
     """
     01. Load data
     """
     # Load tokenizer
-    tokenizer = get_tokenizer(config['TRAINER']['tokenizer'], config['TRAINER']['pretrained'])
+    tokenizer = get_tokenizer(
+        config["TRAINER"]["tokenizer"], config["TRAINER"]["pretrained"]
+    )
 
     # Load Dataset
-    if not DEBUG and os.path.isfile(os.path.join(PREPROCESSED_DIR, 'train_dataset.pt')):
-        train_dataset = torch.load(os.path.join(PREPROCESSED_DIR,'train_dataset.pt'))
-        val_dataset = torch.load(os.path.join(PREPROCESSED_DIR,'val_dataset.pt'))
+    if not DEBUG and os.path.isfile(os.path.join(PREPROCESSED_DIR, "train_dataset.pt")):
+        train_dataset = torch.load(os.path.join(PREPROCESSED_DIR, "train_dataset.pt"))
+        val_dataset = torch.load(os.path.join(PREPROCESSED_DIR, "val_dataset.pt"))
         logger.info("loaded existing .pt")
     else:
-        train_dataset = QADataset(data_dir=os.path.join(DATA_DIR, 'train.json'), tokenizer = tokenizer, max_seq_len = tokenizer.model_max_length, mode = 'train')
-        val_dataset = QADataset(data_dir=os.path.join(DATA_DIR, 'train.json'), tokenizer = tokenizer, max_seq_len = tokenizer.model_max_length, mode = 'val')
+        train_dataset = QADataset(
+            data_dir=os.path.join(DATA_DIR, "train.json"),
+            tokenizer=tokenizer,
+            max_seq_len=tokenizer.model_max_length,
+            mode="train",
+        )
+        val_dataset = QADataset(
+            data_dir=os.path.join(DATA_DIR, "train.json"),
+            tokenizer=tokenizer,
+            max_seq_len=tokenizer.model_max_length,
+            mode="val",
+        )
         if not DEBUG:
             os.makedirs(PREPROCESSED_DIR, exist_ok=True)
-            torch.save(train_dataset, os.path.join(PREPROCESSED_DIR,'train_dataset.pt'))
-            torch.save(val_dataset, os.path.join(PREPROCESSED_DIR,'val_dataset.pt'))
+            torch.save(
+                train_dataset, os.path.join(PREPROCESSED_DIR, "train_dataset.pt")
+            )
+            torch.save(val_dataset, os.path.join(PREPROCESSED_DIR, "val_dataset.pt"))
         logger.info("loaded data, created .pt")
 
-    
     if DEBUG:
         print(len(train_dataset), len(val_dataset))
         for i in range(10):
-            txt = train_dataset[i]['input_ids']
-            start_idx = train_dataset[i]['start_positions']
-            end_idx = train_dataset[i]['end_positions']
+            txt = train_dataset[i]["input_ids"]
+            start_idx = train_dataset[i]["start_positions"]
+            end_idx = train_dataset[i]["end_positions"]
             print(tokenizer.decode(txt[start_idx:end_idx]))
-    
+
     # DataLoader
-    train_dataloader = DataLoader(dataset=train_dataset,
-                                  batch_size=config['DATALOADER']['batch_size'],
-                                  num_workers=config['DATALOADER']['num_workers'], 
-                                  shuffle=config['DATALOADER']['shuffle'],
-                                  pin_memory=config['DATALOADER']['pin_memory'],
-                                  drop_last=config['DATALOADER']['drop_last'])
-    val_dataloader = DataLoader(dataset=val_dataset,
-                                batch_size=config['DATALOADER']['batch_size'],
-                                num_workers=config['DATALOADER']['num_workers'], 
-                                shuffle=False,
-                                pin_memory=config['DATALOADER']['pin_memory'],
-                                drop_last=config['DATALOADER']['drop_last'])
+    train_dataloader = DataLoader(
+        dataset=train_dataset,
+        batch_size=config["DATALOADER"]["batch_size"],
+        num_workers=config["DATALOADER"]["num_workers"],
+        shuffle=config["DATALOADER"]["shuffle"],
+        pin_memory=config["DATALOADER"]["pin_memory"],
+        drop_last=config["DATALOADER"]["drop_last"],
+    )
+    val_dataloader = DataLoader(
+        dataset=val_dataset,
+        batch_size=config["DATALOADER"]["batch_size"],
+        num_workers=config["DATALOADER"]["num_workers"],
+        shuffle=False,
+        pin_memory=config["DATALOADER"]["pin_memory"],
+        drop_last=config["DATALOADER"]["drop_last"],
+    )
 
     logger.info(f"Load data, train:{len(train_dataset)} val:{len(val_dataset)}")
 
@@ -117,59 +133,75 @@ if __name__ == '__main__':
     02. Set model
     """
     # Load model
-    model_name = config['TRAINER']['model']
-    model = get_model(model_name=model_name, pretrained=config['TRAINER']['pretrained']).to(device)
+    model_name = config["TRAINER"]["model"]
+    model = get_model(
+        model_name=model_name, pretrained=config["TRAINER"]["pretrained"]
+    ).to(device)
 
     """
     03. Set trainer
     """
     # Optimizer
-    optimizer = get_optimizer(optimizer_name=config['TRAINER']['optimizer'])
-    optimizer = optimizer(params=model.parameters(),lr=config['TRAINER']['learning_rate'])
+    optimizer = get_optimizer(optimizer_name=config["TRAINER"]["optimizer"])
+    optimizer = optimizer(
+        params=model.parameters(), lr=config["TRAINER"]["learning_rate"]
+    )
 
     # Loss
-    loss = get_loss(loss_name=config['TRAINER']['loss'])
-    
+    loss = get_loss(loss_name=config["TRAINER"]["loss"])
+
     # Metric
-    metrics = {metric_name: get_metric(metric_name) for metric_name in config['TRAINER']['metric']}
-    
+    metrics = {
+        metric_name: get_metric(metric_name)
+        for metric_name in config["TRAINER"]["metric"]
+    }
+
     # Early stoppper
-    early_stopper = EarlyStopper(patience=config['TRAINER']['early_stopping_patience'],
-                                mode=config['TRAINER']['early_stopping_mode'],
-                                logger=logger)
+    early_stopper = EarlyStopper(
+        patience=config["TRAINER"]["early_stopping_patience"],
+        mode=config["TRAINER"]["early_stopping_mode"],
+        logger=logger,
+    )
     # AMP
-    if config['TRAINER']['amp'] == True:
+    if config["TRAINER"]["amp"] == True:
         from apex import amp
-        model, optimizer = amp.initialize(model, optimizer, opt_level='O1')
+
+        model, optimizer = amp.initialize(model, optimizer, opt_level="O1")
 
     # Trainer
-    trainer = Trainer(model=model,
-                      optimizer=optimizer,
-                      loss=loss,
-                      metrics=metrics,
-                      device=device,
-                      logger=logger,
-                      tokenizer=tokenizer,
-                      amp=amp if config['TRAINER']['amp'] else None,
-                      interval=config['LOGGER']['logging_interval'])
-    
+    trainer = Trainer(
+        model=model,
+        optimizer=optimizer,
+        loss=loss,
+        metrics=metrics,
+        device=device,
+        logger=logger,
+        tokenizer=tokenizer,
+        amp=amp if config["TRAINER"]["amp"] else None,
+        interval=config["LOGGER"]["logging_interval"],
+    )
+
     """
     Logger
     """
     # Recorder
-    recorder = Recorder(record_dir=RECORDER_DIR,
-                        model=model,
-                        optimizer=optimizer,
-                        scheduler=None,
-                        amp=amp if config['TRAINER']['amp'] else None,
-                        logger=logger)
+    recorder = Recorder(
+        record_dir=RECORDER_DIR,
+        model=model,
+        optimizer=optimizer,
+        scheduler=None,
+        amp=amp if config["TRAINER"]["amp"] else None,
+        logger=logger,
+    )
 
     # !Wandb
-    USE_WANDB = config['LOGGER']['wandb']['use']
+    USE_WANDB = config["LOGGER"]["wandb"]["use"]
     if USE_WANDB:
-        wandb_project_serial = config['LOGGER']['wandb']['project_serial']
-        wandb_username =  config['LOGGER']['wandb']['username']
-        wandb.init(project=wandb_project_serial, dir=RECORDER_DIR, entity=wandb_username)
+        wandb_project_serial = config["LOGGER"]["wandb"]["project_serial"]
+        wandb_username = config["LOGGER"]["wandb"]["username"]
+        wandb.init(
+            project=wandb_project_serial, dir=RECORDER_DIR, entity=wandb_username
+        )
         wandb.run.name = train_serial
         wandb.config.update(config)
         wandb.watch(model)
@@ -181,47 +213,47 @@ if __name__ == '__main__':
     04. TRAIN
     """
     # Train
-    n_epochs = config['TRAINER']['n_epochs']
+    n_epochs = config["TRAINER"]["n_epochs"]
     for epoch_index in range(n_epochs):
 
         # Set Recorder row
         row_dict = dict()
-        row_dict['epoch_index'] = epoch_index
-        row_dict['train_serial'] = train_serial
-        
+        row_dict["epoch_index"] = epoch_index
+        row_dict["train_serial"] = train_serial
+
         """
         Train
         """
         print(f"Train {epoch_index}/{n_epochs}")
         logger.info(f"--Train {epoch_index}/{n_epochs}")
         trainer.train(dataloader=train_dataloader, epoch_index=epoch_index)
-        
-        row_dict['train_loss'] = trainer.loss_mean
-        row_dict['train_elapsed_time'] = trainer.elapsed_time 
-        
+
+        row_dict["train_loss"] = trainer.loss_mean
+        row_dict["train_elapsed_time"] = trainer.elapsed_time
+
         for metric_str, score in trainer.score_dict.items():
             row_dict[f"train_{metric_str}"] = score
         trainer.clear_history()
-        
+
         """
         Validation
         """
         print(f"Val {epoch_index}/{n_epochs}")
         logger.info(f"--Val {epoch_index}/{n_epochs}")
         trainer.validate(dataloader=val_dataloader, epoch_index=epoch_index)
-        
-        row_dict['val_loss'] = trainer.loss_mean
-        row_dict['val_elapsed_time'] = trainer.elapsed_time 
-        
+
+        row_dict["val_loss"] = trainer.loss_mean
+        row_dict["val_elapsed_time"] = trainer.elapsed_time
+
         for metric_str, score in trainer.score_dict.items():
             row_dict[f"val_{metric_str}"] = score
         trainer.clear_history()
-        
+
         """
         Record
         """
         recorder.add_row(row_dict)
-        recorder.save_plot(config['LOGGER']['plot'])
+        recorder.save_plot(config["LOGGER"]["plot"])
 
         #!WANDB
         if USE_WANDB:
@@ -230,17 +262,18 @@ if __name__ == '__main__':
         """
         Early stopper
         """
-        early_stopping_target = config['TRAINER']['early_stopping_target']
+        early_stopping_target = config["TRAINER"]["early_stopping_target"]
         early_stopper.check_early_stopping(loss=row_dict[early_stopping_target])
 
         if early_stopper.patience_counter == 0:
             recorder.save_weight(epoch=epoch_index)
             best_row_dict = copy.deepcopy(row_dict)
-        
+
         if early_stopper.stop == True:
-            logger.info(f"Early stopped, counter {early_stopper.patience_counter}/{config['TRAINER']['early_stopping_patience']}")
-            
+            logger.info(
+                f"Early stopped, counter {early_stopper.patience_counter}/{config['TRAINER']['early_stopping_patience']}"
+            )
+
             if USE_WANDB:
                 wandb.log(best_row_dict)
             break
-
